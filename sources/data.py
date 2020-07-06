ca051c3aff6c702bdf8e8a438518574861bb7a00
Masoud Mokhtari <mmokhtari@triumf.ca>
2020-07-05 21:46:24 -0700

    Made changes to dataset iterator and collate function

--- sources/data.py
+++ sources/data.py
@@ -3,6 +3,10 @@ import numpy as np
 import scipy.sparse as sp
 import networkx as nx
 import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+import h5py
+import torch
+import dgl
 
 
 def create_knn_adj_mat(features, k, weighted=False, n_jobs=None):
@@ -96,3 +100,79 @@ def draw_graph(adj_mat, weighted=False, directed=False):
     # Show the graph
     plt.title('Graph with Spring Layout')
     plt.show()
+
+
+def collate(samples):
+    """
+    Collate function used by the data loader to put graphs into a batch
+    """
+    graphs, labels = map(list, zip(*samples))
+    batched_graph = dgl.batch(graphs)
+    return batched_graph, torch.reshape(torch.tensor(labels, dtype=torch.float), [-1, 1])
+
+
+class ProstateCancerDataset(Dataset):
+    """
+    Dataset class for the prostate cancer dataset
+    """
+
+    def __init__(self, mat_file_path, train=True, weighted=False, k=10):
+        """
+        Constructor for the prostate cancer dataset class
+        Parameters:
+            mat_file_path (str): Path to the .mat file
+            train (bool): Indicates whether train or test data is loaded
+            weighted (bool): Indicates whether created graph is weighted or not
+            k (int): Number of neighbours to use for the K-nearest neighbour algorithm
+        """
+
+        # Load the .mat file
+        self.prostate_cancer_mat_data = h5py.File(mat_file_path, 'r')
+
+        # Load either the test or train data
+        self.mat_data = self.prostate_cancer_mat_data['data_train' if train else 'data_test']
+
+        # Find the number of available cells
+        self.num_cells = self.mat_data.shape[0]
+
+        # Obtain the labels for the cells
+        self.labels = np.array(self.prostate_cancer_mat_data['label_train' if train else 'label_test'], dtype=np.int)
+
+        # Parameters used in graph creation
+        self.weighted = weighted
+        self.k = k
+
+    def __getitem__(self, idx):
+        """
+        Item iterator for the prostate cancer dataset
+        Parameters:
+            idx (int): index of data point to retrieve
+        Returns:
+            (numpy array): Numpy array containing the signals for a single cell
+            (int): Label indicating whether the cell is cancerous or healthy
+        """
+
+        # Obtain the label for the specified cell
+        label = self.labels[idx][0]
+
+        # Obtain the cell signals and change them into a numpy array
+        data = np.array(self.prostate_cancer_mat_data[self.mat_data[idx, 0]][()].transpose(), dtype=np.float32)
+
+        # Create the graph using knn
+        graph = create_knn_adj_mat(data, k=self.k, weighted=self.weighted)
+
+        # Create a dgl graph from coo_matrix
+        g = dgl.DGLGraph()
+        g.from_scipy_sparse_matrix(graph)
+
+        # Put RF signals as node features
+        g.nodes[:].data['x'] = data
+
+        return g, label
+
+    def __len__(self):
+        """
+        Returns:
+            (int): Indicates the number of available cells
+        """
+        return self.num_cells
